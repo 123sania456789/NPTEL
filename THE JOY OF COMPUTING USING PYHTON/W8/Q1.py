37de19c95bbb71ab89bbda87cb4dfff9e0c7a978
SANIA <20p0245sania@gmail.com>
2025-03-20 23:00:36 +0530

    Q1.py

--- THE JOY OF COMPUTING USING PYHTON/W8/Q1.py
+++ THE JOY OF COMPUTING USING PYHTON/W8/Q1.py
@@ -1,3 +1,6 @@
-abc
-
-
+input_list = eval(input())
+result = []
+for tup in input_list:
+  new_tuple = tuple(item for item in tup if not isinstance(item, str))
+  result.append(new_tuple)
+print(result, end='')
