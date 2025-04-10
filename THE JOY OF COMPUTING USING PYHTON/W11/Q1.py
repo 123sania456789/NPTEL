a768c1770547e028bd12d244cf872ebd743ce60c
SANIA <20p0245sania@gmail.com>
2025-04-10 20:52:14 +0530

    Update Q1.py

--- THE JOY OF COMPUTING USING PYHTON/W11/Q1.py
+++ THE JOY OF COMPUTING USING PYHTON/W11/Q1.py
@@ -1,13 +1,3 @@
-auto Print = [&RecMin](std::vector<int> tVec) {
-            std::cout << RecMin(tVec, tVec.size()); 
-        };
-        int n, m;
-        std::vector<int> vec;
-        std::cin >> n;
-        for(int i = 0; i < n; i++){
-            std::cin >> m;
-            vec.push_back(m);
-        }
-        Print(vec);
-        return 0;
-    }
+def missingNumber(nums):
+    return(sum(range(len(nums)+1))-sum(nums))
+
